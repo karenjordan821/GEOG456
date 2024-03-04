a84834cd91bbb8d47c3dc216ba4c462abd7c5198
karenjordan821 <156230956+karenjordan821@users.noreply.github.com>
2024-03-04 16:29:13 -0500

    attempt 1

---
+++ hw7/data/arlene.js
@@ -0,0 +1,21 @@
+var arlene = {
+    "type":"FeatureCollection", 
+    "name": "hurricane",
+    "features":[
+{"type":"Feature","geometry":null,"properties":{"Obs":5,"Date":"06/09","Latitude":20.2,"Longitude":-84.2,"Move (nm)":null,"Speed(nm/hr)":null,"Error (nm)":"0.0"}},
+{"type":"Feature","geometry":null,"properties":{"Obs":6,"Date":"06/09","Latitude":20.9,"Longitude":-84.3,"Move (nm)":44.1,"Speed(nm/hr)":7.4,"Error (nm)":"7.7"}},
+{"type":"Feature","geometry":null,"properties":{"Obs":7,"Date":"06/10","Latitude":21.6,"Longitude":-84.5,"Move (nm)":41.1,"Speed(nm/hr)":6.9,"Error (nm)":"18.4"}},
+{"type":"Feature","geometry":null,"properties":{"Obs":8,"Date":"06/10","Latitude":22.2,"Longitude":-84.6,"Move (nm)":39.7,"Speed(nm/hr)":6.6,"Error (nm)":"106.5"}},
+{"type":"Feature","geometry":null,"properties":{"Obs":9,"Date":"06/10","Latitude":22.8,"Longitude":-84.8,"Move (nm)":37.8,"Speed(nm/hr)":6.3,"Error (nm)":"172.4"}},
+{"type":"Feature","geometry":null,"properties":{"Obs":10,"Date":"06/10","Latitude":23.4,"Longitude":-85,"Move (nm)":36.4,"Speed(nm/hr)":6.1,"Error (nm)":"***"}},
+{"type":"Feature","geometry":null,"properties":{"Obs":11,"Date":"06/11","Latitude":24,"Longitude":-85.3,"Move (nm)":35.1,"Speed(nm/hr)":5.9,"Error (nm)":"***"}},
+{"type":"Feature","geometry":null,"properties":{"Obs":12,"Date":"06/11","Latitude":24.5,"Longitude":-85.5,"Move (nm)":34.3,"Speed(nm/hr)":5.7,"Error (nm)":"***"}},
+{"type":"Feature","geometry":null,"properties":{"Obs":13,"Date":"06/11","Latitude":25,"Longitude":-85.8,"Move (nm)":33.6,"Speed(nm/hr)":5.6,"Error (nm)":"***"}},
+{"type":"Feature","geometry":null,"properties":{"Obs":14,"Date":"06/11","Latitude":25.5,"Longitude":-86,"Move (nm)":30.3,"Speed(nm/hr)":5,"Error (nm)":"***"}},
+{"type":"Feature","geometry":null,"properties":{"Obs":15,"Date":"06/12","Latitude":25.9,"Longitude":-86.2,"Move (nm)":28,"Speed(nm/hr)":4.7,"Error (nm)":"***"}},
+{"type":"Feature","geometry":null,"properties":{"Obs":16,"Date":"06/12","Latitude":26.3,"Longitude":-86.3,"Move (nm)":26.6,"Speed(nm/hr)":4.4,"Error (nm)":"***"}},
+{"type":"Feature","geometry":null,"properties":{"Obs":17,"Date":"06/12","Latitude":26.7,"Longitude":-86.5,"Move (nm)":25.8,"Speed(nm/hr)":4.3,"Error (nm)":"***"}},
+{"type":"Feature","geometry":null,"properties":{"Obs":18,"Date":"06/12","Latitude":27.1,"Longitude":-86.6,"Move (nm)":25.1,"Speed(nm/hr)":4.2,"Error (nm)":"***"}},
+{"type":"Feature","geometry":null,"properties":{"Obs":19,"Date":"06/13","Latitude":27.5,"Longitude":-86.7,"Move (nm)":24.4,"Speed(nm/hr)":4.1,"Error (nm)":"***"}},
+{"type":"Feature","geometry":null,"properties":{"Obs":20,"Date":"06/13","Latitude":27.9,"Longitude":-86.8,"Move (nm)":23.9,"Speed(nm/hr)":4,"Error (nm)":"***"}}
+]}
